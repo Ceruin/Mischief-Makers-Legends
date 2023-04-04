a6d1f49af3ce9fc5dc1d3d3a07e7a7790ce1b91c
Ceruin <13532471+Ceruin@users.noreply.github.com>
2023-04-04 00:32:12 -0400

    .

--- Mischief-Makers-Legends/Assets/Scripts/PlayerMovement.cs
+++ Mischief-Makers-Legends/Assets/Scripts/PlayerMovement.cs
@@ -4,65 +4,152 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 10f;
-    [SerializeField] private float rotationSpeed = 360f;
-    [SerializeField] private float turnSpeed = 10f;
-    [SerializeField] private float backwardSpeedRatio = 0.5f;
-    [SerializeField] private float groundDashSpeed = 20f;
-    [SerializeField] private float airDashSpeed = 15f;
-    [SerializeField] private float dashDuration = 0.2f;
-    [SerializeField] private float dashCooldown = 0.5f;
-    [SerializeField] private float grabDistance = 2f;
-    [SerializeField] private float throwForce = 10f;
-    [SerializeField] private float shakeDuration = 1f;
-    [SerializeField] private float shakeAngle = 15f;
-    [SerializeField] private float shakeSpeed = 50f;
-    [SerializeField] private float jumpForce = 10f;
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private float groundCheckDistance = 0.2f;
-    [SerializeField] private Vector3 groundCheckOffset = new Vector3(0, -1, 0);
-    [SerializeField] private Transform playerModel;
+    public float airHoverSpeed = 15f;
+    public float backwardSpeedRatio = 0.5f;
+    public float dashForce = 20f; // Force applied during the dash
+    public float dashDuration = 0.2f; // Duration of the dash
+    public float dashCooldown = 0.5f; // Cooldown between dashes
+    public float dashDistanceMultiplier = 2f; // Multiplier to increase the dash distance based on current momentum
+    public float fuelConsumptionRate = 1f; // Fuel consumption rate per second
+    public float fuelRegenerationRate = 0.5f; // Fuel regeneration rate per second
+    public float grabDistance = 2f;
+    public float groundCheckDistance = 0.2f;
+    public Vector3 groundCheckOffset = new Vector3(0, -1, 0);
+    public float groundHoverSpeed = 20f;
+    public LayerMask groundLayer;
+    public float hoverDuration = 0.1f;
+    public float hoverForce = 5f;
+    public float jumpForce = 10f;
+    public float jumpHoverDelay = 0.5f;
+    public float maxFuel = 3f; // Maximum fuel for the jetpack
+    public float moveSpeed = 10f;
+    public Transform playerModel;
+    public float rotationSpeed = 360f;
+    public float shakeAngle = 15f;
+    public float shakeDuration = 1f;
+    public float shakeSpeed = 50f;
+    public float throwForce = 10f;
+    public float turnSpeed = 10f;
 
-    private Rigidbody rb;
-    private Vector2 moveInput;
-
-
-    private bool isDashing = false;
-    private float timeSinceLastDash = 0f;
+    private bool isDashing = false; // Is the player currently dashing
+    private float currentFuel;
     private GameObject grabbedObject;
+    private bool isHovering = false;
+    private Vector2 moveInput;
+    private Rigidbody rb;
+    private float timeSinceLastDash; // Time since the last dash
+    private float timeSinceLastHover = 0f;
+    private float timeSinceLastJump;
+
+    private IEnumerator Dash(Vector3 dashDirection)
+    {
+        if (isDashing || Time.time - timeSinceLastDash < dashCooldown) yield break;
+
+        isDashing = true;
+        timeSinceLastDash = Time.time;
+
+        float dashEndTime = Time.time + dashDuration;
+        rb.AddForce(dashDirection * dashForce, ForceMode.VelocityChange);
+
+        while (Time.time < dashEndTime)
+        {
+            yield return null;
+        }
+
+        isDashing = false;
+    }
+
+    public void OnDashInput(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Vector3 dashDirection = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
+            StartCoroutine(Dash(dashDirection));
+        }
+    }
+
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (IsGrounded())
+            {
+                Jump();
+            }
+            else
+            {
+                if (Time.time - timeSinceLastJump >= jumpHoverDelay)
+                {
+                    StartCoroutine(Hover());
+                }
+            }
+        }
+    }
+
+    public void OnMovementInput(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnMovementInputCanceled(InputAction.CallbackContext context)
+    {
+        moveInput = Vector2.zero;
+    }
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-    }
-
-    private void Start()
-    {
-        PlayerActions actions = new PlayerActions();
-
-        actions.Movement.Movement.performed += OnMovementInput;
-        actions.Movement.Movement.canceled += OnMovementInputCanceled;
-        actions.Movement.Jump.performed += OnJumpInput;
-        actions.Movement.Run.performed += OnDashInput;
-
-        actions.Enable();
-    }
-
-    private void Update()
-    {
-        if (IsGrounded())
-        {
-            playerModel.GetComponent<Renderer>().material.color = Color.green;
-        }
-        else
-        {
-            playerModel.GetComponent<Renderer>().material.color = Color.red;
-        }
+        currentFuel = maxFuel;
     }
 
     private void FixedUpdate()
     {
         Move();
+    }
+
+    private void GrabObject()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(playerModel.position, playerModel.forward, out hit, grabDistance) && hit.collider.gameObject.layer == LayerMask.NameToLayer("Grabbable"))
+        {
+            grabbedObject = hit.collider.gameObject;
+            grabbedObject.transform.SetParent(playerModel);
+            grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+
+    private IEnumerator Hover()
+    {
+        if (isHovering || Time.time - timeSinceLastJump < jumpHoverDelay || currentFuel <= 0) yield break;
+
+        isHovering = true;
+        timeSinceLastHover = Time.time;
+
+        float hoverEndTime = Time.time + hoverDuration;
+        currentFuel -= fuelConsumptionRate * hoverDuration;
+
+        while (Time.time < hoverEndTime)
+        {
+            rb.AddForce(new Vector3(0f, hoverForce, 0f), ForceMode.Acceleration);
+            yield return null;
+        }
+
+        isHovering = false;
+    }
+
+    private bool IsGrounded()
+    {
+        Vector3 checkPosition = transform.position + groundCheckOffset;
+        return Physics.CheckSphere(checkPosition, groundCheckDistance, groundLayer);
+    }
+
+    private void Jump()
+    {
+        if (IsGrounded())
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            timeSinceLastJump = Time.time;
+        }
     }
 
     private void Move()
@@ -78,33 +165,12 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private IEnumerator Dash(Vector3 dashDirection)
+    private void RegenerateFuel()
     {
-        if (isDashing || Time.time - timeSinceLastDash < dashCooldown) yield break;
-
-        isDashing = true;
-        timeSinceLastDash = Time.time;
-
-        float dashEndTime = Time.time + dashDuration;
-        float currentDashSpeed = IsGrounded() ? groundDashSpeed : airDashSpeed;
-
-        while (Time.time < dashEndTime)
+        if (!isHovering && currentFuel < maxFuel)
         {
-            rb.velocity = new Vector3(dashDirection.x * currentDashSpeed, dashDirection.y * currentDashSpeed, dashDirection.z * currentDashSpeed);
-            yield return null;
-        }
-
-        isDashing = false;
-    }
-
-    private void GrabObject()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(playerModel.position, playerModel.forward, out hit, grabDistance) && hit.collider.gameObject.layer == LayerMask.NameToLayer("Grabbable"))
-        {
-            grabbedObject = hit.collider.gameObject;
-            grabbedObject.transform.SetParent(playerModel);
-            grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
+            currentFuel += fuelRegenerationRate * Time.deltaTime;
+            currentFuel = Mathf.Clamp(currentFuel, 0, maxFuel);
         }
     }
 
@@ -133,44 +199,28 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    public void OnMovementInput(InputAction.CallbackContext context)
+    private void Start()
     {
-        moveInput = context.ReadValue<Vector2>();
+        PlayerActions actions = new PlayerActions();
+
+        actions.Movement.Movement.performed += OnMovementInput;
+        actions.Movement.Movement.canceled += OnMovementInputCanceled;
+        actions.Movement.Jump.performed += OnJumpInput;
+        actions.Movement.Run.performed += OnDashInput;
+
+        actions.Enable();
     }
 
-    public void OnMovementInputCanceled(InputAction.CallbackContext context)
-    {
-        moveInput = Vector2.zero;
-    }
-
-    private bool IsGrounded()
-    {
-        Vector3 checkPosition = transform.position + groundCheckOffset;
-        return Physics.CheckSphere(checkPosition, groundCheckDistance, groundLayer);
-    }
-
-    private void Jump()
+    private void Update()
     {
         if (IsGrounded())
         {
-            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            playerModel.GetComponent<Renderer>().material.color = Color.green;
+            RegenerateFuel();
         }
-    }
-
-    public void OnJumpInput(InputAction.CallbackContext context)
-    {
-        if (context.performed)
+        else
         {
-            Jump();
-        }
-    }
-
-    public void OnDashInput(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            Vector3 dashDirection = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
-            StartCoroutine(Dash(dashDirection));
+            playerModel.GetComponent<Renderer>().material.color = Color.red;
         }
     }
 }
