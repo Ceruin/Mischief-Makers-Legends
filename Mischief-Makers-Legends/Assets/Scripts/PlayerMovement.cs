bda24b2674c2c52298848b68cfe7ae9f29e93528
Ceruin <13532471+Ceruin@users.noreply.github.com>
2023-04-22 22:03:57 -0400

    .

--- Mischief-Makers-Legends/Assets/Scripts/PlayerMovement.cs
+++ Mischief-Makers-Legends/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -103,8 +104,15 @@ public class PlayerMovement : MonoBehaviour
         moveInput = Vector2.zero;
     }
 
+    // cinemachine
+    private float _cinemachineTargetYaw;
+
+    private float _cinemachineTargetPitch;
+
     private void Awake()
     {
+        _cinemachineTargetYaw = CinemachineCameraTarget.transform.rotation.eulerAngles.y;
+
         rb = GetComponent<Rigidbody>();
         currentFuel = maxFuel;
     }
@@ -159,6 +167,69 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private bool IsCurrentDeviceMouse
+    {
+        get
+        {
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+                return _playerInput.currentControlScheme == "KeyboardMouse";
+#else
+            return false;
+#endif
+        }
+    }
+
+    private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
+    {
+        if (lfAngle < -360f) lfAngle += 360f;
+        if (lfAngle > 360f) lfAngle -= 360f;
+        return Mathf.Clamp(lfAngle, lfMin, lfMax);
+    }
+
+    [Tooltip("How far in degrees can you move the camera up")]
+    public float TopClamp = 70.0f;
+
+    [Tooltip("Additional degress to override the camera. Useful for fine tuning camera position when locked")]
+    public float CameraAngleOverride = 0.0f;
+
+    [Header("Cinemachine")]
+    [Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
+    public GameObject CinemachineCameraTarget;
+
+    [Tooltip("How far in degrees can you move the camera down")]
+    public float BottomClamp = -30.0f;
+
+    [Tooltip("For locking the camera position on all axis")]
+    public bool LockCameraPosition = false;
+
+    private const float _threshold = 0.01f;
+
+    private void LateUpdate()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        // if there is an input and camera position is not fixed
+        if (!LockCameraPosition)
+        {
+            //Don't multiply mouse input by Time.deltaTime;
+            float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
+
+            _cinemachineTargetYaw += deltaTimeMultiplier;
+            _cinemachineTargetPitch += deltaTimeMultiplier;
+        }
+
+        // clamp our rotations so our values are limited 360 degrees
+        _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
+        _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+
+        // Cinemachine will follow this target
+        CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride,
+            _cinemachineTargetYaw, 0.0f);
+    }
+
     private void Move()
     {
         Vector3 moveDirection = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
@@ -172,10 +243,20 @@ public class PlayerMovement : MonoBehaviour
         Vector3 desiredDirection = cameraForward * moveDirection.z + cameraRight * moveDirection.x;
         rb.velocity = new Vector3(desiredDirection.x * moveSpeed, rb.velocity.y, desiredDirection.z * moveSpeed);
 
-        if (desiredDirection != Vector3.zero)
+        if (desiredDirection != Vector3.zero && desiredDirection.magnitude > 0.1f)
         {
-            Quaternion targetRotation = Quaternion.LookRotation(desiredDirection, Vector3.up);
-            rb.MoveRotation(Quaternion.RotateTowards(rb.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime));
+            //Quaternion targetRotation = Quaternion.LookRotation(desiredDirection.normalized, Vector3.up);
+            //rb.MoveRotation(Quaternion.RotateTowards(rb.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime));
+            //rb.MoveRotation(Quaternion.LookRotation(desiredDirection));
+
+            Quaternion targetRotation = Quaternion.LookRotation(desiredDirection.normalized, Vector3.up);
+            var diferenceRotation = targetRotation.eulerAngles.y - transform.eulerAngles.y;
+            var eulerY = transform.eulerAngles.y;
+
+            if (diferenceRotation < 0 || diferenceRotation > 0) eulerY = targetRotation.eulerAngles.y;
+            var euler = new Vector3(0, eulerY, 0);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(euler), turnSpeed * rotationSpeed * Time.deltaTime);
         }
     }
 
